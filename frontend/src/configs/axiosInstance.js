ddd08bb55eb69293e4f074a2e191f52f378cd96e
Nguyen Trung Kien <nguyentrungkien040921@gmail.com>
2026-03-20 22:02:02 +0700

    change baseURL frontend

--- frontend/src/configs/axiosInstance.js
+++ frontend/src/configs/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://manage-restaurant-94p4.onrender.com",
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
   withCredentials: true,
 });
 
