cebf83e0a6a860af38954db569fcb385d1e16702
Nguyen Trung Kien <nguyentrungkien040921@gmail.com>
2026-03-21 11:42:52 +0700

    update baseURl frontend

--- frontend/src/configs/axiosInstance.js
+++ frontend/src/configs/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://manage-restaurant-94p4.onrender.com",
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
